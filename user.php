b1da935f2e469f43435361183fe84cbfec9a8e45
MarioPazmino <93871082+MarioPazmino@users.noreply.github.com>
2024-07-29 14:39:26 -0500

    Update user.php

--- user.php
+++ user.php
@@ -383,22 +383,21 @@ $empleado = $collection->findOne(['_id' => $usuario_id]);
                                     </span>
                                 </td>
 
-<td>
-                                    <form method="post" action="actualizar_tarea.php" class="d-flex align-items-center">
-                                        <input type="hidden" name="tarea_index" value="<?= $index ?>">
-                                        <input type="hidden" name="tarea_id" value="<?= $tarea['_id'] ?>">
-                                         <div class="form-group mb-0 mr-4">
-                                            <select name="estado" class="form-control">
-                                                <option value="Pendiente" <?= $tarea['estado'] === 'Pendiente' ? 'selected' : '' ?>>Pendiente</option>
-                                                <option value="En Progreso" <?= $tarea['estado'] === 'En Progreso' ? 'selected' : '' ?>>En Progreso</option>
-                                                <option value="Completada" <?= $tarea['estado'] === 'Completada' ? 'selected' : '' ?>>Completada</option>
-                                            </select>
-                                        </div>
-                                        <button type="submit" class="btn btn-primary">Actualizar</button>
-                                    </form>
-                                </td>
+<td class="text-center">
+    <form method="post" action="actualizar_tarea.php" class="d-inline-flex align-items-center">
+        <input type="hidden" name="tarea_index" value="<?= $index ?>">
+        <input type="hidden" name="tarea_id" value="<?= $tarea['_id'] ?>">
+        <div class="form-group mb-0 mr-2">
+            <select name="estado" class="form-control">
+                <option value="Pendiente" <?= $tarea['estado'] === 'Pendiente' ? 'selected' : '' ?>>Pendiente</option>
+                <option value="En Progreso" <?= $tarea['estado'] === 'En Progreso' ? 'selected' : '' ?>>En Progreso</option>
+                <option value="Completada" <?= $tarea['estado'] === 'Completada' ? 'selected' : '' ?>>Completada</option>
+            </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Actualizar</button>
+    </form>
+</td>
 
-                            </tr>
                         <?php endforeach; ?>
                     </tbody>
                 </table>

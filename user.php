c3d860a72293a8bc0262175859da222d6ce3057a
MarioPazmino <93871082+MarioPazmino@users.noreply.github.com>
2024-07-29 14:36:16 -0500

    Update user.php

--- user.php
+++ user.php
@@ -384,7 +384,7 @@ $empleado = $collection->findOne(['_id' => $usuario_id]);
                                 </td>
 
 <td>
-                                    <form method="post" action="actualizar_tarea.php">
+                                    <form method="post" action="actualizar_tarea.php" class="d-flex align-items-center">
                                         <input type="hidden" name="tarea_index" value="<?= $index ?>">
                                         <input type="hidden" name="tarea_id" value="<?= $tarea['_id'] ?>">
                                          <div class="form-group mb-0 mr-2">

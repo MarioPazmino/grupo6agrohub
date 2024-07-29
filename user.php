b9dddd38b471f3bf9bce03b92151b625fcdbd901
MarioPazmino <93871082+MarioPazmino@users.noreply.github.com>
2024-07-29 13:49:15 -0500

    Update user.php

--- user.php
+++ user.php
@@ -318,7 +318,7 @@ $empleado = $collection->findOne(['_id' => $usuario_id]);
 <div class="container-fluid">
 
     <!-- Page Heading -->
-    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+    <div class="d-sm-flex align-items-center justify-content-between mb-12">
         <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
         
     </div>

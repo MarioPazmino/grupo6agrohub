1184980586941c2962b5715e177b58d59ec14b90
MarioPazmino <93871082+MarioPazmino@users.noreply.github.com>
2024-07-29 19:48:05 -0500

    Update terrrenos.php

--- terrrenos.php
+++ terrrenos.php
@@ -380,7 +380,7 @@ $total_tareas_completadas = $collection->countDocuments([
         <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
             <!-- Sidebar - Brand -->
-            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="user.php">
+            <a class="sidebar-brand d-flex align-items-center justify-content-center" href="admin.php">
                 <div class="sidebar-brand-icon rotate-n-15">
                     <i class="fas fa-laugh-wink"></i>
                 </div>

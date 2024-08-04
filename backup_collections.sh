945f0a37941348a571072a6961e81b864e400113
MarioPazmino <mrpazmino2@espe.edu.ec>
2024-08-04 04:15:41 +0000

    Descripción de los cambios realizados

---
+++ backup_collections.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Define the database and the output directory
+DB_NAME="grupo6_agrohub"
+OUTPUT_DIR="/var/www/html/grupo6agrohub/backup"
+URI="mongodb://mario1010:marito10@testmongo1.cluster-c9ccw6ywgi5c.us-east-1.docdb.amazonaws.com:27017/?tls=true&tlsCAFile=/var/www/html/grupo6agrohub/global-bundle.pem"
+
+# Array of collections
+COLLECTIONS=("cosechas" "productos" "siembras" "terrenos" "usuarios")
+
+# Export each collection to JSON
+for COLLECTION in "${COLLECTIONS[@]}"; do
+    echo "Exporting collection: $COLLECTION"
+    mongoexport --uri="$URI" --db="$DB_NAME" --collection="$COLLECTION" --out="$OUTPUT_DIR/$COLLECTION.json"
+done
+
+echo "Backup completed."
